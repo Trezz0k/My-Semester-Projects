77b051c30c34822071cebda24fa23f5474e5710a
Trezz0k <loneal2@cps.edu>
2025-01-17 09:36:36 -0600

    Add files via upload

---
+++ 99loops (1).py
@@ -0,0 +1,20 @@
+#LatrezO'Neal
+#10/24/2024
+def bottles():
+    milk=99
+    print(str(milk)+ "bottles of....")
+    y=("take one down pass it around")
+    milk=milk-1
+
+
+#VariablesX=string for milk lyric
+#VariablesB=String for milk lyric
+#variablesY=Take one down pass it down lyric
+#variablesmilk=milk-1
+    for i in range(99):
+     x=(str(milk)+ " bottles of milk on the wall.")
+     b=(str(milk)+ " bottles of milk on the wall.")
+     y=("take one down pass it around")
+     print (x+b+y)
+     milk=milk- 1
+bottles()
